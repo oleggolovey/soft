272c1dcb1d114e543a3615a0cc31fa1733051eab
oleggolovey <oledzho1998@gmail.com>
2021-04-23 18:41:17 +0300

    init

--- js/main.js
+++ js/main.js
@@ -100,3 +100,24 @@ window.onscroll = function () {
   //fixedBtn.style.display = "none";
   //}
 };
+
+// jQuery form sending
+$(document).ready(function () {
+  //E-mail Ajax Send
+  $("#form").submit(function () {
+    //Change
+    var th = $(this);
+    $.ajax({
+      type: "POST",
+      url: "mail.php", //Change
+      data: th.serialize(),
+    }).done(function () {
+      alert("Thank you!");
+      setTimeout(function () {
+        // Done Functions
+        th.trigger("reset");
+      }, 1000);
+    });
+    return false;
+  });
+});
